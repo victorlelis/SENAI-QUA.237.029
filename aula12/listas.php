b306f9caf08b734b70b121e30928b6e50815918f
Victor Lelis <ovictorlelis@gmail.com>
2022-07-27 21:43:30 -0300

    aula12

---
+++ aula12/listas.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Listas</title>
+</head>
+
+<body>
+
+  <!-- lista não ordenada -->
+  <ul>
+    <li>Francisco</li>
+    <li>João</li>
+    <li>Maria</li>
+    <li>Paulo</li>
+    <li>Marcos</li>
+  </ul>
+
+  <!-- lista ordenada -->
+  <ol>
+    <li>Honda</li>
+    <li>Fiat</li>
+    <li>Toyota</li>
+    <li>Ford</li>
+    <li>Hyundai</li>
+  </ol>
+
+  <ol type="a">
+    <li>Ubuntu</li>
+    <li>Fedora</li>
+    <li>Red Hat</li>
+    <li>PopOS</li>
+    <li>Debian</li>
+  </ol>
+
+  <ol type="i">
+    <li>Brasília</li>
+    <li>São Paulo</li>
+    <li>Goiânia</li>
+    <li>Belém</li>
+  </ol>
+
+</body>
+
+</html>
---
+++ aula12/tabelas.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tabelas</title>
+</head>
+
+<body>
+
+  <table>
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Telefone</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>Victor</td>
+        <td>ovictorlelis@gmail.com</td>
+        <td>(61) 9000-000</td>
+      </tr>
+    </tbody>
+  </table>
+
+</body>
+
+</html>
